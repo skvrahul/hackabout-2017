fb12b4a225742fa5f1579c9d1d6f32fd66c05940
Vishnu Dasu <vishnu98dasu@gmail.com>
2017-08-18 23:24:28 +0530

    Add POS and stem

--- sentence.py
+++ sentence.py
@@ -3,7 +3,9 @@ class Sentence:
 	e2 = None
 	sentence = []
 	nominal_distance = None
-
+	pos_nominals = ()
+	pos_words = ()
+	stem_words = ()
 	"""
 	Constructor for the sentence datatype
 
@@ -11,15 +13,21 @@ class Sentence:
 	    nominals: Tuple (e1, e2)
 	    sentence: Array of strings representing the entire sentence
 	    nominal_distance: Number of words between the nominals 
+        pos_nominals: POS tags of nominals
+        pos_words: POS tags of words between nominals
+        stem_words: Stems of words between nominals
 
 	Returns:
 	    Returns a Sentence obj
 	"""
-	def __init__(self,nominals, sentence, nominal_distance):
+	def __init__(self,nominals, sentence, nominal_distance,pos_nominals,pos_words,stem_words):
 		self.e1=nominals[0]
 		self.e2=nominals[1]
 		self.sentence = sentence
 		self.nominal_distance = nominal_distance
+		self.pos_nominals = pos_nominals
+		self.pos_words = pos_words
+		self.stem_words = stem_words
 	
 	def get_nominals(self):
 		return (self.e1,self.e2)
