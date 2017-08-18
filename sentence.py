3d87ea3ba08f930798391a16186db745bbc8655e
Rahul Sathanapalli <skvrahul@gmail.com>
2017-08-18 12:21:01 +0530

    Updated code to comply with python3

--- sentence.py
+++ sentence.py
@@ -3,20 +3,11 @@ class Sentence:
 	e2 = None
 	sentence = []
 	nominal_distance = None
-	def __init__(self):
-		pass
-	
-	def __init__(self,(e1,e2), sentence):
-		self.e1=e1
-		self.e2=e2
-		self.sentence = sentence
-
-	def __init__(self,(e1,e2), sentence, nominal_distance):
-		self.e1=e1
-		self.e2=e2
+	def __init__(self,nominals, sentence, nominal_distance):
+		self.e1=nominals[0]
+		self.e2=nominals[1]
 		self.sentence = sentence
 		self.nominal_distance = nominal_distance
 	
 	def get_nominals(self):
-		return (e1,e2)
-			
+		return (self.e1,self.e2)
