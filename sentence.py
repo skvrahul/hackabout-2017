2885eab094dadf4aa56cbfab0f51ecc3dfbf9db6
Rahul Sathanapalli <skvrahul@gmail.com>
2017-08-20 12:05:33 +0530

    Fixes errors in sentence.py

--- sentence.py
+++ sentence.py
@@ -1,14 +1,4 @@
 class Sentence:
-	e1 = None
-	e2 = None
-	sentence = []
-	nominal_distance = None
-	pos_nominals = ()
-	pos_words = ()
-	stem_words = ()
-	label= None
-	index_e1 = None
-	index_e2 = None
 
 	"""
 	Constructor for the sentence datatype
@@ -41,8 +31,8 @@ class Sentence:
 	def create_feature_dict(self):
 			feature_dict = {'e1':self.e1, 'e2':self.e2}
 			feature_dict.update({'words:'+sentence_word:True for sentence_word in self.sentence})
-			feature_dict.update({'nom_dist':nominal_distance})
-			feature_dict.update({'nom_pos:'+pos_nominal for pos_nominal in pos_nominals})
-			feature_dict.update({'words_pos:'+pos_word for pos_word in pos_words})
-			feature_dict.update({'words_stem:'+stem_word for stem_word in stem_words})
+			feature_dict.update({'nom_dist':self.nominal_distance})
+			feature_dict.update({'nom_pos:'+pos_nominal for pos_nominal in self.pos_nominals})
+			feature_dict.update({'words_pos:'+pos_word for pos_word in self.pos_words})
+			feature_dict.update({'words_stem:'+stem_word for stem_word in self.stem_words})
 			return feature_dict
