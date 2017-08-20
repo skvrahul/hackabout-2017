767498895cdabdbf1e6740724ec10054c72a0f19
Rahul Sathanapalli <skvrahul@gmail.com>
2017-08-20 12:19:06 +0530

    Adds sentence_dict()  in sentence.py

--- sentence.py
+++ sentence.py
@@ -29,10 +29,15 @@ class Sentence:
 		return (self.e1,self.e2)
 
 	def create_feature_dict(self):
-			feature_dict = {'e1':self.e1, 'e2':self.e2}
-			feature_dict.update({'words:'+sentence_word:True for sentence_word in self.sentence})
-			feature_dict.update({'nom_dist':self.nominal_distance})
-			feature_dict.update({'nom_pos:'+pos_nominal:True for pos_nominal in self.pos_nominals})
-			feature_dict.update({'words_pos:'+pos_word:True for pos_word in self.pos_words})
-			feature_dict.update({'words_stem:'+stem_word:True for stem_word in self.stem_words})
-			return feature_dict
+		feature_dict = {'e1':self.e1, 'e2':self.e2}
+		feature_dict.update({'words:'+sentence_word:True for sentence_word in self.sentence})
+		feature_dict.update({'nom_dist':self.nominal_distance})
+		feature_dict.update({'nom_pos:'+pos_nominal:True for pos_nominal in self.pos_nominals})
+		feature_dict.update({'words_pos:'+pos_word:True for pos_word in self.pos_words})
+		feature_dict.update({'words_stem:'+stem_word:True for stem_word in self.stem_words})
+		return feature_dict
+
+	def sentence_dict(self)
+		word_dict = {'words:'+word:True for word in sentence} 		
+		return word_dict
+	
